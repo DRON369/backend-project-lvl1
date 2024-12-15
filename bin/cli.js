a37086142527fc972cd03c360ef47f4f3be5495f
Andrey Makhmutov <dron369@ya.ru>
2024-12-15 21:35:09 +0500

    feat: Add eslint packages and configuration. Fix linter issues.

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export function greeting() {
+function greeting() {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name?\n');
   console.log(`Hello, ${username}!`);
 }
+
+export default greeting;
